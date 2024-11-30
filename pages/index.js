1fb744c1302a38a6501d9ee44652b3dd5d399fa9
André Arruda <andre23arruda@gmail.com>
2024-11-30 15:07:13 +0000

    feat: First challenge

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+function Home() {
+    return (
+        <div style={{margin: 20}}>
+            <h1>Olá, queridona</h1>
+            <h3>Esse é o primeiro desafio do curso: Fazer o deploy de uma página para uma pessoa especial</h3>
+            <ul>
+                <li>Espero realmente que esse curso seja incrível e que eu possa aprender mais coisas.</li>
+                <li>Ou também absorver um pouco da energia do Filipe, pois acho ele fantástico.</li>
+            </ul>
+            <u>Você é a pessoal especial na minha vida e quero sempre estar com você <i>(em dias de luta e em dias de glória)</i></u>
+            <p>Te amo ❤️</p>
+        </div>
+    )
+}
+
+export default Home
